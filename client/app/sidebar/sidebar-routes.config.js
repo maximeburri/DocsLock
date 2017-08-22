7e3c241de70d99458a5c1000261bde8159a979c2
Maxime Burri <maxime.burri@gmail.com>
2017-08-22 14:50:07 +0200

    Fixed untracked files

--- client/app/sidebar/sidebar-routes.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.ROUTES = [
-    { path: 'devices', title: 'Devices', icon: 'tablet_android', class: '' },
-    { path: 'files', title: 'Files', icon: 'folder', class: '' },
-    { path: 'user', title: 'User Profile', icon: 'person', class: '' },
-];
-//# sourceMappingURL=sidebar-routes.config.js.map
