c4ebebb89d6b488252407e76c424597aa4122d10
Maxime Burri <maxime.burri@gmail.com>
2017-08-22 14:32:48 +0200

    Add routes/pages devices and files

---
+++ client/app/dashboard/devices/devices.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'devices-cmp',
+    moduleId: module.id,
+    templateUrl: 'devices.component.html'
+})
+
+export class DevicesComponent implements OnInit{
+    ngOnInit(){
+
+    }
+}
---
+++ client/app/dashboard/files/files.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'files-cmp',
+    moduleId: module.id,
+    templateUrl: 'files.component.html'
+})
+
+export class FilesComponent implements OnInit{
+    ngOnInit(){
+
+    }
+}
