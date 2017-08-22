975702ae38aecd8367d246fa4ab207a8dda67e56
Maxime Burri <maxime.burri@gmail.com>
2017-08-22 16:18:19 +0200

    Add sails server

---
+++ server/api/controllers/DeviceController.js
@@ -0,0 +1,26 @@
+/**
+ * DeviceController
+ *
+ * @description :: Server-side logic for managing Devices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    create: function (req, res) {
+
+        let mac = req.param('mac');
+
+        if (!mac) {
+            return res.badRequest({ err: 'Invalid mac' });
+        }
+
+        Device.create({
+            mac: mac,
+        }).then(_device => {
+            if (!_device) return res.serverError({ err: 'Unable to create device' });
+
+            return res.ok(_device);
+        }).catch(err => res.serverError(err.message));
+    }
+};
+
---
+++ server/api/controllers/PoDeviceController.js
@@ -0,0 +1,21 @@
+/**
+ * PoDeviceController
+ *
+ * @description :: Server-side logic for managing Podevices
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PoDeviceController.create()`
+   */
+  create: function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  }
+};
+
