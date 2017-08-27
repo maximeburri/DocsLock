8068d9880c471f965db75b9150ec1520d03b2ccd
Maxime Burri <maxime.burri@gmail.com>
2017-08-27 14:01:44 +0200

    Add first template for devices list

--- client/app/dashboard/dashboard.module.ts
+++ client/app/dashboard/dashboard.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MODULE_COMPONENTS, MODULE_ROUTES } from './dashboard.routes';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
-        RouterModule.forChild(MODULE_ROUTES)
+        RouterModule.forChild(MODULE_ROUTES),
+        CommonModule
     ],
     declarations: [ MODULE_COMPONENTS ]
 })
--- client/app/dashboard/devices/devices.component.ts
+++ client/app/dashboard/devices/devices.component.ts
@@ -7,7 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class DevicesComponent implements OnInit{
-    ngOnInit(){
+    public devices = [];
 
+    ngOnInit(){
+        this.devices = [
+            {
+                "id" : 1,
+                "os" : "android",
+                "type" : "tablet",
+                "mac" : "AA::BB::CC::DD::EE"
+            },
+            {
+                "id" : 2,
+                "os" : "android",
+                "type" : "tablet",
+                "mac" : "AA::BB::CC::DD::EE"
+            },
+            {
+                "id" : 3,
+                "os" : "android",
+                "type" : "tablet",
+                "mac" : "AA::BB::CC::DD::EE"
+            },
+            {
+                "id" : 4,
+                "os" : "android",
+                "type" : "tablet",
+                "mac" : "AA::BB::CC::DD::EE"
+            }
+        ];
     }
 }
