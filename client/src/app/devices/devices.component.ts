aad44136d9d9a5c2750f90df5f8ed405ddb7e074
Maxime Burri <maxime.burri@gmail.com>
2017-08-31 13:49:35 +0200

    Add connection to websocket
    Add the disable.js file, else sails.io connect to the same port (4200),
    and say an error (server is on port 1337).

--- client/src/app/devices/devices.component.ts
+++ client/src/app/devices/devices.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SailsService } from "angular2-sails";
+declare var io;
 
 @Component({
     selector: 'devices-cmp',
@@ -13,7 +14,18 @@ export class DevicesComponent implements OnInit {
     constructor(private _sailsService: SailsService) { }
 
     ngOnInit() {
-        this._sailsService.connect("http://localhost:1337");
+        this._sailsService
+            .connect("http://127.0.0.1:1337")
+            .subscribe(
+                t => console.log("Connected"),
+                e => console.error(e)
+            );
+
+        io.socket.get('/device', function(data, jwr) {
+            io.socket.on('new_entry', function(entry) {
+            });
+        });
+
 
         /* device : 
         {
---
+++ client/src/scripts/disable.js
@@ -0,0 +1,2 @@
+// Disable sails.io autoconnect else he try to connect to the same address/port
+io.sails.autoConnect=false;
