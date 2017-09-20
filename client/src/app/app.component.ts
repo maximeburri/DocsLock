57e03be45c592c5ecb552a42b3a13d20bbff64bb
Maxime Burri <maxime.burri@gmail.com>
2017-09-20 16:35:49 +0200

    Fix checkbox bugs

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+
+declare const $: any;
 
 @Component({
   selector: 'app-root',
@@ -8,19 +10,21 @@ import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common'
 })
 export class AppComponent implements OnInit {
 
-  constructor(public location: Location) {}
+  constructor(public location: Location) { }
 
   ngOnInit() {
+    $.material.options.autofill = true;
+    $.material.init();
   }
 
-    isMaps(path){
-      var titlee = this.location.prepareExternalUrl(this.location.path());
-      titlee = titlee.slice( 1 );
-      if(path == titlee){
-        return false;
-      }
-      else {
-        return true;
-      }
+  isMaps(path){
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    titlee = titlee.slice( 1 );
+    if(path == titlee){
+      return false;
     }
+    else {
+      return true;
+    }
+  }
 }
