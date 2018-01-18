2e98b78bfba34a28c969666bdd643a5084fc2f77
Maxime Burri <maxime.burri@gmail.com>
2018-01-18 14:50:45 +0100

    Set server IP

--- android-app/app/src/main/java/ch/burci/docslock/Config.java
+++ android-app/app/src/main/java/ch/burci/docslock/Config.java
@@ -7,6 +7,6 @@ package ch.burci.docslock;
 public class Config {
     public final static String SERVER_PROTCOL = "http";
     // public final static String SERVER_IP_PORT = "192.168.1.118:1337"; // Wifi Assistants
-    public final static String SERVER_IP_PORT = "192.168.178.53:1337"; // Wifi Maison
+    public final static String SERVER_IP_PORT = "192.168.1.2:1337"; // Wifi Goreptar
     public final static String SERVER_URL = SERVER_PROTCOL + "://" + SERVER_IP_PORT;
 }
